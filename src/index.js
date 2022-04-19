dc0adb84bc1f64fd3148b833a0ea628d1844dcae
jello0097 <jello0097@hanmail.net>
2022-04-19 10:48:45 +0900

    prettier 적용, jsx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const root = document.getElementById("root");
-const span = React.createElement("span", {
-    key: "wanna-go-home-span"
-}, "I wanna go home");
-const btn = React.createElement("button", {
-    key: "btn",
-    onClick: () => {
-        console.log('I am clicked')
-    }
-}, "Click me");
-const contaniner = React.createElement("div", null, [span, btn]);
+const root = document.getElementById('root');
 
-ReactDOM.render(contaniner, root);
+const Title = () => {
+  return (
+    <h3
+      id="title"
+      onMouseEnter={() => {
+        console.log('mouse enter');
+      }}
+    >
+      Hello This is Title
+    </h3>
+  );
+};
+
+const Button = () => {
+  return (
+    <button
+      style={{ backgroundColor: 'orange' }}
+      onClick={() => {
+        console.log('I am clicked');
+      }}
+    >
+      Click me!
+    </button>
+  );
+};
+
+const Container = () => {
+  return (
+    <div>
+      <Title />
+      <Button />
+    </div>
+  );
+};
+
+ReactDOM.render(<Container />, root);
