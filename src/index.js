8e3e46e0bf6b366f7dc28854b6f0ed51a62f69f7
jello0097 <jello0097@hanmail.net>
2022-04-19 18:26:16 +0900

    setState 4  - super multi convertor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const root = document.getElementById('root');
-
-const App = () => {
+function MinutesToHours() {
   const [amount, setAmount] = React.useState(0);
   const [inverted, setInverted] = React.useState(false);
   const onChange = (event) => {
@@ -14,10 +12,8 @@ const App = () => {
     reset();
     setInverted((current) => !current);
   };
-
   return (
     <div>
-      <h1>Super Convertor</h1>
       <label htmlFor="minutes">Minutes</label>
       <input
         value={inverted ? amount * 60 : amount}
@@ -40,6 +36,32 @@ const App = () => {
       <button onClick={onInvert}>{inverted ? 'Trun back' : 'Invert'}</button>
     </div>
   );
-};
+}
 
+function KmToMiles() {
+  return <h3>Km to Miles</h3>;
+}
+
+function App() {
+  const [index, setIndex] = React.useState('not-selected');
+  const onSelect = (event) => {
+    setIndex(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Super Convertor</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="not-selected">Select plz!</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km t& Miles</option>
+      </select>
+      <hr />
+      {index === 'not-selected' ? 'Select unit please!' : null}
+      {index === '0' ? <MinutesToHours /> : null}
+      {index === '1' ? <KmToMiles /> : null}
+    </div>
+  );
+}
+
+const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
