e80a1ebcbcf75b725554c35fccef8e8a05dc7b09
jello0097 <jello0097@hanmail.net>
2022-04-22 16:34:10 +0900

    useEffect 2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
   useEffect(() => {
-    console.log('I run only once.');
+    console.log('new');
+    return () => console.log('destory'); // cleanup
   }, []);
-  useEffect(() => {
-    console.log('I run when keyword changes.');
-  }, [keyword]);
-  useEffect(() => {
-    console.log('I run when counter changes.');
-  }, [counter]);
-  useEffect(() => {
-    console.log('I run when keyword & counter changes');
-  }, [counter, keyword]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [show, setShow] = useState(false);
+  const onClick = () => setShow((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Seacrh here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <div>
+        {show ? <Hello /> : null}
+        <button onClick={onClick}>{show ? 'hide' : 'show'}</button>
+      </div>
     </div>
   );
 }
