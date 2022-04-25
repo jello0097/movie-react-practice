cabaefcc890a454283495f9f2db5021940414144
jello0097 <jello0097@hanmail.net>
2022-04-25 14:55:34 +0900

    coins - fetch and render list

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To do list ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={toDo}
-          onChange={onChange}
-          placeholder="Write your to do.."
-        />
-        <button>submit</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select>
+        {coins.map((coin) => (
+          <option key={coin.id}>
+            {coin.name} ({coin.symbol}: {coin.quotes.USD.price}) USD
+          </option>
         ))}
-      </ul>
+      </select>
     </div>
   );
 }
