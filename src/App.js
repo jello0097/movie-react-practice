03089747683f5ea65463a39f0976a52d8a4fc889
jello0097 <jello0097@hanmail.net>
2022-04-25 14:27:02 +0900

    To do list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import { useState, useEffect } from 'react';
 
-function Hello() {
-  useEffect(() => {
-    console.log('new');
-    return () => console.log('destory'); // cleanup
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [show, setShow] = useState(false);
-  const onClick = () => setShow((prev) => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
   return (
     <div>
-      <div>
-        {show ? <Hello /> : null}
-        <button onClick={onClick}>{show ? 'hide' : 'show'}</button>
-      </div>
+      <h1>My To do list ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={toDo}
+          onChange={onChange}
+          placeholder="Write your to do.."
+        />
+        <button>submit</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
 export default App;
